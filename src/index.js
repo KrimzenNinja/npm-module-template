8ae288099b8421bf02221679c8ea3ec5e708fb86
Ryan Kotzen <potz666@gmail.com>
2017-08-03 15:54:31 +0200

    Adding debug lib to encourage usage in modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+const debug = require('debug')('krimzen-ninja-module-template');
 /**
  * This function says hello.
  * @param name Some name to say hello to.
- * @returns The hello message.
+ * @returns {string} The hello message.
  */
-const sayHello = (name = 'World') => `Hello, ${name}!`;
+const sayHello = (name = 'World') => {
+    debug('say hello called with %s', name);
+    return `Hello, ${name}!`;
+};
 
 export default sayHello;
