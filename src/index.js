efbd5e4d50e01d9bd4d533891ed265601ebe585c
Ryan Kotzen <potz666@gmail.com>
2017-08-03 14:05:54 +0200

    removing flow

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-// @flow
-
 /**
  * This function says hello.
  * @param name Some name to say hello to.
  * @returns The hello message.
  */
-const sayHello = (name: string = 'World'): string => `Hello, ${name}!`;
+const sayHello = (name = 'World') => `Hello, ${name}!`;
 
 export default sayHello;
